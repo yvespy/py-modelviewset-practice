fdb8954d8fcd043135226fb7371aa0e139cb4485
yevpy <eugene2piskun@gmail.com>
2025-04-20 02:40:01 +0300

    Solution

--- author/serializers.py
+++ author/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
 
+from author.models import Author
+
 
 class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+    class Meta:
+        model = Author
+        fields = ["id",
+                  "first_name",
+                  "last_name",
+                  "pseudonym",
+                  "age",
+                  "retired"
+                  ]
--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,12 @@
-# Create your urls here
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from author.views import AuthorViewSet
+
+router = DefaultRouter()
+router.register(r"", AuthorViewSet, basename="author")
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "author"
